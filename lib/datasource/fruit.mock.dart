319e9c468ae966e3d187c94e28cee6c19328e4ad
Breno <brenotosi14@gmail.com>
2024-08-26 20:51:59 -0300

    lista de bagre feita

---
+++ lib/datasource/fruit.mock.dart
@@ -0,0 +1,10 @@
+import 'package:fruitstore/domain/fruit.dart';
+
+final fruitMock = [
+  Fruit("Yuri Alber", "R\$10,00"),
+  Fruit("Pedro Raul", "R\$20,00"),
+  Fruit("Romero", "R\$30,00"),
+  Fruit("Hugo Bota", "R\$40,00"),
+  Fruit("Yuri dnv", "R\$50,00"),
+  Fruit("Yogurte Aberto", "R\$70,00"),
+];
